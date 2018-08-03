9083cb854420411ea22f9328e8e01742d5dc4ad1
Vinícius Lourenço Santos <vinicius.lourenco@gmail.com>
2018-08-03 03:55:59 -0300

    startup

---
+++ MyComparer/IMyEqualityComparer.cs
@@ -0,0 +1,12 @@
+﻿namespace MyComparer
+{
+    using System.Collections.Generic;
+
+    public interface IMyEqualityComparer<T>: IEqualityComparer<T>
+    {
+        IEnumerable<T> Equals(IEnumerable<T> items, T item);
+        IEnumerable<T> Equals(T item, IEnumerable<T> items);
+        IEnumerable<T> Duplicates(IEnumerable<T> items);
+        IEnumerable<T> Distincts(IEnumerable<T> items);
+    }
+}
---
+++ MyComparer/MyExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace MyComparer
+{
+    using System.Collections.Generic;
+    using MyList.DAL.Entities;
+
+    public static class MyExtension
+    {
+        public static IEnumerable<File> Duplicates(
+            this IEnumerable<File> files, 
+            IMyEqualityComparer<File> comparer)
+            => comparer.Duplicates(files);
+    }
+}
